cec6e53308aa3f65c40a2154d177994934708acd
Vinicius <viniciusdarosapadilha@gmail.com>
2025-05-21 15:11:37 -0300

    Guarda o username e password em uma variavel

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -13,7 +13,18 @@ class AuthController extends Controller
 
     public function loginSubmit( Request $request)
     {
-        echo 'login Submit';
+        $request->validate(
+            [
+                'text_username' => 'required',
+                'text_password' => 'required'
+            ]
+        );
+
+        // get user input
+        $username = $request->input('text_username');
+        $password = $request->input('text_password');
+
+        echo 'OK!';
     }
 
     public function logout()
